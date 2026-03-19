9e465d299234dacc6a173b86c535619f5b8d8dd8
Clinton Nkefuat <nkefuatclintonngu@gmail.com>
2026-03-19 04:35:17 -0500

    Added first exploration SQL script

---
+++ sql/01_exploration.sql
@@ -0,0 +1,7 @@
+-- ============================================
+-- File: 01_exploration.sql
+-- Description: Initial exploration of FactInternetSales
+-- ============================================
+
+SELECT TOP 100 *
+FROM dbo.FactInternetSales;
